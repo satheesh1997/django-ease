f869eef23811a9f6f28b27a06bf0fee4de8b0f8a
Satheesh Kumar <satheesh.101097@gmail.com>
2020-10-26 20:31:02 +0530

    decore/models: adds UniqueSlug and Slug abstract models with method to generate slug

--- decore/models.py
+++ decore/models.py
@@ -3,7 +3,10 @@ This file contains all the abstract models that can make
 a django developers work easy.
 """
 
+from django.db.signals import post_save
 from django.db import models
+from django.dispatch import receiver
+from django.template.defaultfilters import slugify
 
 
 class AbstractModel(models.Model):
@@ -27,3 +30,53 @@ class TimeStamps(AbstractModel):
     """
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class UniqueSlug(AbstractModel):
+    """
+    Abstract model that can be inherited to have a unique slug field
+    in the model.
+
+    When inhertied will check for slug_from attribute in the model,
+    by default slug_from will have value title.
+
+    """
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta(AbstractModel.Meta):
+        slug_from = 'title'
+
+    def _generate_slug(self):
+        title = getattr(self, self.Meta.slug_from)
+
+        if title:
+            new_slug = slugify(title)
+            slug_exists = self.__class__.objects.filter(slug=new_slug).exists()
+
+            if slug_exists:
+                new_slug = "{}-{}".format(new_slug, self.id)
+
+            self.slug = new_slug
+            self.save()
+
+
+class Slug(AbstractModel):
+    """
+    Abstract model that can be inherited to have a slug field
+    in the model.
+
+    When inhertied will check for slug_from attribute in the model,
+    by default slug_from will have value title.
+
+    """
+    slug = models.SlugField(max_length=200, db_index=True)
+
+    class Meta(AbstractModel.Meta):
+        slug_from = 'title'
+
+    def _generate_slug(self):
+        title = getattr(self, self.Meta.slug_from)
+
+        if title:
+            self.slug = slugify(title)
+            self.save()
